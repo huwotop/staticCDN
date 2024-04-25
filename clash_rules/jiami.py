b8bae301ac513113180835e43eb31a886811518e
huwotop <gxkakuken@gmail.com>
2024-04-25 16:10:07 +0800

    更新

--- clash_rules/jiami.py
+++ clash_rules/jiami.py
@@ -2,7 +2,15 @@ import base64
 import requests
 from lxml import etree
 
-v = "vmess://ewogICJ2IjogIjIiLAogICJwcyI6ICJKUOiHquW7uuiKgueCuSIsCiAgImFkZCI6ICJkZW1vLmh1d28udG9wIiwKICAicG9ydCI6ICI0NDMiLAogICJpZCI6ICI3MUQwRjI1RS0xMEQ5LTZERDAtM0UwRi04ODUxODFGMjRBREQiLAogICJhaWQiOiAiMCIsCiAgInNjeSI6ICJhdXRvIiwKICAibmV0IjogIndzIiwKICAidHlwZSI6ICJub25lIiwKICAiaG9zdCI6ICIiLAogICJwYXRoIjogIi9odXdvMjA1MHRvcCIsCiAgInRscyI6ICJ0bHMiLAogICJzbmkiOiAiIiwKICAiYWxwbiI6ICIiLAogICJmcCI6ICIiCn0\n"
+v = 
+"vless://196b6c47-41c9-4f96-8fee-1238e983597e@158.101.132.255:8881?encryption=none&security=reality&sni=addons.mozilla.org&fp=chrome&pbk=GPnzQut0UNo4wpSR2IV_2XsVUwnsbqzwCzwPcPc4-nM&type=tcp&headerType=none#虎窝节点%20xtls-reality\n
+hysteria2://196b6c47-41c9-4f96-8fee-1238e983597e@158.101.132.255:8882/?alpn=h3&insecure=1#虎窝节点%20hysteria2\n
+tuic://196b6c47-41c9-4f96-8fee-1238e983597e:196b6c47-41c9-4f96-8fee-1238e983597e@158.101.132.255:8883?alpn=h3&congestion_control=bbr#虎窝节点%20tuic\n
+ss://YWVzLTEyOC1nY206MTk2YjZjNDctNDFjOS00Zjk2LThmZWUtMTIzOGU5ODM1OTdlQDE1OC4xMDEuMTMyLjI1NTo4ODg1#虎窝节点%20shadowsocks\n
+trojan://196b6c47-41c9-4f96-8fee-1238e983597e@158.101.132.255:8886?security=tls&type=tcp&headerType=none#虎窝节点%20trojan\n
+vmess://eyAidiI6ICIyIiwgInBzIjogIuiZjueqneiKgueCuSB2bWVzcy13cyIsICJhZGQiOiAiY2RuLWIxMDAueG4tLWI2Z2FjLmV1Lm9yZyIsICJwb3J0IjogIjgwIiwgImlkIjogIjE5NmI2YzQ3LTQxYzktNGY5Ni04ZmVlLTEyMzhlOTgzNTk3ZSIsICJhaWQiOiAiMCIsICJzY3kiOiAibm9uZSIsICJuZXQiOiAid3MiLCAidHlwZSI6ICJub25lIiwgImhvc3QiOiAiZGVtby5odXdvLnRvcCIsICJwYXRoIjogIi8xOTZiNmM0Ny00MWM5LTRmOTYtOGZlZS0xMjM4ZTk4MzU5N2Utdm1lc3MiLCAidGxzIjogIiIsICJzbmkiOiAiIiwgImFscG4iOiAiIiB9\n
+vless://196b6c47-41c9-4f96-8fee-1238e983597e@cdn-b100.xn--b6gac.eu.org:443?encryption=none&security=tls&sni=demo.huwo.top&type=ws&host=demo.huwo.top&path=%2F196b6c47-41c9-4f96-8fee-1238e983597e-vless%3Fed%3D2048#虎窝节点%20vless-ws-tls\n
+vless://196b6c47-41c9-4f96-8fee-1238e983597e@158.101.132.255:8890?encryption=none&security=reality&sni=addons.mozilla.org&fp=chrome&pbk=GPnzQut0UNo4wpSR2IV_2XsVUwnsbqzwCzwPcPc4-nM&type=grpc&serviceName=grpc&mode=gun#虎窝节点%20grpc-reality\n"
 
 def ipQuery(ip):    
     url = "https://browserleaks.com/ip/" + ip
